a38eac0d19822fb85f79d5b8807473fbb97742a1
Jantwel <jantwel.me@gmail.com>
2020-06-18 20:33:18 +0300

    optimize hook

--- src/game-state/hook.js
+++ src/game-state/hook.js
@@ -4,7 +4,7 @@ import { getInitialState, worldTick } from './index';
 import { TICK_INTERVAL } from '../constants';
 
 export const useGameOfLife = () => {
-  const [gameState, setGameState] = React.useState(getInitialState());
+  const [gameState, setGameState] = React.useState(getInitialState);
 
   React.useEffect(() => {
     let timerId = setInterval(() => setGameState(worldTick), TICK_INTERVAL);
